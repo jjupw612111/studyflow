4f21c887d0622222118c81b470bd49360f027282
Amy Liao <amzhliao@gmail.com>
2024-12-04 16:14:22 -0600

    todos merge

--- server/merge/lambda_function.py
+++ server/merge/lambda_function.py
@@ -10,6 +10,7 @@ import datatier
 
 from configparser import ConfigParser
 
+# /merge/{userid}/{projectid}
 def lambda_handler(event, context):
   try:
     print("**STARTING**")
@@ -34,27 +35,69 @@ def lambda_handler(event, context):
     rds_dbname = configur.get('rds', 'db_name')
 
     #
+    # configure for s3 access
+    #
+    s3_profile = 's3readwrite'
+    boto3.setup_default_session(profile_name=s3_profile)
+    
+    bucketname = configur.get('s3', 'bucket_name')
+    
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucketname) 
+
+    #
+    # get userid from path
+    #
+    print("**Accessing event/pathParameters**")
+
+    if "userid" in event:
+      userid = event["userid"]
+    elif "pathParameters" in event:
+      if "userid" in event["pathParameters"]:
+        userid = event["pathParameters"]["userid"]
+      else:
+        raise Exception("requires userid parameter in pathParameters")
+    else:
+        raise Exception("requires userid parameter in event") 
+    print("userid:", userid)
+
+    #
+    # get body of request:
+    # - projectname
+    #
+    print("**Accessing request body**") 
+
+    if "body" not in event:
+      raise Exception("event has no body") 
+    body = json.loads(event["body"]) # parse the json 
+    if "projectname" not in body:
+      raise Exception("event has a body but no projectname")
+    projectname = body["projectname"]    
+    print("projectname:", projectname)
+
+
+    #
     # open connection to the database:
     #
-    print("**Opening connection**")
-    
+    print("**Opening connection**") 
     dbConn = datatier.get_dbConn(rds_endpoint, rds_portnum, rds_username, rds_pwd, rds_dbname)
     
     #
-    # now retrieve all the users:
+    # TODO: get s3 bucket folder for the project
     #
-    print("**Retrieving data**")
 
     #
-    # TODO #1 of 1: write sql query to select all users from the 
-    # users table, ordered by userid
+    # TODO: download each pdf at a time, merge it
     #
-    sql = "SELECT * from users ORDER BY userid";
     
-    rows = datatier.retrieve_all_rows(dbConn, sql)
-    
-    for row in rows:
-      print(row)
+    #
+    # TODO: resulting merged pdf uploaded to s3
+    # in results folder
+    #
+
+    #
+    # TODO: return the s3 download url
+    # 
 
     #
     # respond in an HTTP-like way, i.e. with a status
