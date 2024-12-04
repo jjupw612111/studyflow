03e5bbf19afe7e6469b6b4c5f8fa36b25e5f9295
Siddharth Saha <siddharthsaha03@gmail.com>
2024-12-03 18:18:01 -0600

    cheat sheet w text file

--- server/cheatSheet/lambda_function.py
+++ server/cheatSheet/lambda_function.py
@@ -1,20 +1,19 @@
 #
-# Retrieves and returns all the users in the 
-# BenfordApp database.
+# Takes a user input for : list of topics and projectID
+# outputs a cheat sheet pdf
 #
 
 import json
 import boto3
 import os
 import datatier
-
+from openai_helpers import openai_sheet_helper 
 from configparser import ConfigParser
 
 def lambda_handler(event, context):
   try:
     print("**STARTING**")
-    print("**lambda: proj03_users**")
-    
+    print("**lambda: cheatSheet**")
     #
     # setup AWS based on config file:
     #
@@ -23,48 +22,51 @@ def lambda_handler(event, context):
     
     configur = ConfigParser()
     configur.read(config_file)
+
+    # #
+    # # configure for RDS access
+    # #
+    # rds_endpoint = configur.get('rds', 'endpoint')
+    # rds_portnum = int(configur.get('rds', 'port_number'))
+    # rds_username = configur.get('rds', 'user_name')
+    # rds_pwd = configur.get('rds', 'user_pwd')
+    # rds_dbname = configur.get('rds', 'db_name')
+
+    # #
+    # # open connection to the database:
+    # #
+    # print("**Opening connection**")
     
-    #
-    # configure for RDS access
-    #
-    rds_endpoint = configur.get('rds', 'endpoint')
-    rds_portnum = int(configur.get('rds', 'port_number'))
-    rds_username = configur.get('rds', 'user_name')
-    rds_pwd = configur.get('rds', 'user_pwd')
-    rds_dbname = configur.get('rds', 'db_name')
+    # dbConn = datatier.get_dbConn(rds_endpoint, rds_portnum, rds_username, rds_pwd, rds_dbname)
+    
+    #get information from user:
+    print("input the list of topics")
+    topics_list = input()
+    print("input the project ID to retrieve the data from S3")
+    project_id = input()
 
     #
-    # open connection to the database:
-    #
-    print("**Opening connection**")
-    
-    dbConn = datatier.get_dbConn(rds_endpoint, rds_portnum, rds_username, rds_pwd, rds_dbname)
-    
-    #
-    # now retrieve all the users:
+    # now retrieve the text from the conversations table:
     #
     print("**Retrieving data**")
+    #TODO: retrieve the text from the conversations table 
 
-    #
-    # TODO #1 of 1: write sql query to select all users from the 
-    # users table, ordered by userid
-    #
-    sql = "SELECT * from users ORDER BY userid";
+    with open("sample.txt", "r") as file:
+      content = file.read()
+      print(content)
     
-    rows = datatier.retrieve_all_rows(dbConn, sql)
-    
-    for row in rows:
-      print(row)
+    gen_text = openai_sheet_helper(topics_list, content) #we need projectID to get the relevant text for the document 
 
+    # 
     #
     # respond in an HTTP-like way, i.e. with a status
     # code and body in JSON format:
     #
-    print("**DONE, returning rows**")
+    print("**DONE, returning generated text**")
     
     return {
       'statusCode': 200,
-      'body': json.dumps(rows)
+      'body': gen_text
     }
     
   except Exception as err:
---
+++ server/cheatSheet/openai_helpers.py
@@ -0,0 +1,33 @@
+import openai 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+topics = "rainforest trees, understory, biodiversity"
+
+def openai_sheet_helper(topics, content):
+  client = openai.OpenAI()
+  completion = client.chat.completions.create(
+      model="gpt-4o",
+      messages=[
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": f'''Below, you are given two pieces of information. First, content for which we want you to make a review sheet.
+            Second, a list of topics for making a review sheet. Please output a review sheet where for each topic, you list the topic
+            and the relevant information about it. 
+            Content: {content}
+            List of Topics: {topics}
+            '''
+          }
+        ]
+      }
+    ]
+  )
+  gentext = completion.choices[0].message.content
+  return gentext
+
